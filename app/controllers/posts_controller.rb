497fba793c44d15a08a98f2b6dc5359b28847275
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-12 03:56:41 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    raise params.inspect
+    # raise params.inspect
     post = Post.create(post_params)
     redirect_to post
   end
